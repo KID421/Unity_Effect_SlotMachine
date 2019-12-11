e9e2b2e438e318d5c5fe34fdcc7f918d921bb10a
KID <50666207+KID421@users.noreply.github.com>
2019-12-11 15:59:19 +0800

    基本設定

---
+++ UnitySlotMachine/Assets/SlotMachine.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlotMachine : MonoBehaviour
+{
+    public RectTransform[] slots;
+
+    public int length;
+
+    public float speed = 10;
+
+    public int index;
+
+    private void Start()
+    {
+        length = transform.childCount;
+        slots = new RectTransform[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            slots[i] = transform.GetChild(i).GetComponent<RectTransform>();
+        }
+
+        index = length - 1;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        for (int i = 0; i < length; i++)
+        {
+            slots[i].Translate(0, -speed * Time.deltaTime, 0);
+
+            if (slots[i].anchoredPosition.y <= -150)
+            {
+                slots[i].anchoredPosition = slots[index].anchoredPosition + new Vector2(0, 100);
+                index = i;
+            }
+        }
+    }
+}
